55e879ddaea7d08051e8ac51f64e818424327364
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-12-26 12:40:37 -0800

    Basic Info Reworked

---
+++ src/components/Hero2/index.tsx
@@ -0,0 +1,29 @@
+import AboutUs from "@/pages/api/AboutUs";
+
+const Hero2 = () => {
+    const scrollIntoFeatures = () => {
+        const options: ScrollIntoViewOptions = {
+            block: 'start',
+            inline: "nearest",
+            behavior: 'smooth'
+        }
+        document.getElementById('features').scrollIntoView(options)
+    }
+
+    return (
+        <section className={"static-hero"}>
+            <div className={"hero-inner"}>
+                <div className={'container'}>
+                    <div data-swiper-parallax={"300"} className={"slide-title text-orange-500"}>
+                        <h2>{AboutUs.slideTitle}</h2>
+                    </div>
+                    <div data-swiper-parallax={"400"} className={'slide-text'}>
+                        <p>{AboutUs.description}</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero2
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import Hero from "@/components/hero";
 import Features from "@/components/Features";
 import PracticeArea from "@/components/PracticeArea";
 import ContactForm from "@/components/ContactForm";
+import Hero2 from "@/components/Hero2";
 
 export default function Home(props) {
     return (
         <Fragment>
             <Navbar Logo={Logo}/>
-            <Hero/>
+            {/*<Hero/>*/}
+            <Hero2/>
             <Features/>
             <PracticeArea/>
             <ContactForm/>
