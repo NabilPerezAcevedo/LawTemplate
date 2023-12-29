e8e5b25cd14ac70c3e0fd4411e39a1fddf9f00c3
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-12-29 08:32:19 -0800

    Change ContactUs Images

--- src/pages/api/ContactFormImages.ts
+++ src/pages/api/ContactFormImages.ts
@@ -1,25 +1,24 @@
-// import jhanselOne from '/public/images/jhanselProfile2/IMG_3562.jpg'
-// import jhanselTwo from '/public/images/jhanselProfile2/IMG_3573.jpg'
-// import jhanselFive from '/public/images/jhanselProfile1/five.jpg'
-import jhanselSix from '/public/images/jhanselProfile1/six.jpg'
-import jhanselFour from '/public/images/jhanselProfile1/four.jpg'
+import One from '../../../public/images/lawStock/1.jpg'
+import Two from '../../../public/images/lawStock/2.jpg'
+import Three from '../../../public/images/lawStock/3.jpg'
+import Four from '../../../public/images/lawStock/4.jpg'
+import Five from '../../../public/images/lawStock/5.jpg'
+import Six from '../../../public/images/lawStock/6.jpg'
 
 const Images = [
-    // {
-    //     original: jhanselOne.src,
-    // },
-    // {
-    //     original: jhanselTwo.src,
-    // }
-    // {
-    //     original: jhanselFive.src,
-    // },
     {
-        original: jhanselSix.src,
-    },
-    {
-        original: jhanselFour.src,
-    },
+        original: One.src,
+    }, {
+        original: Two.src,
+    }, {
+        original: Three.src,
+    }, {
+        original: Four.src,
+    }, {
+        original: Five.src,
+    }, {
+        original: Six.src,
+    }
 ]
 
 export default Images
